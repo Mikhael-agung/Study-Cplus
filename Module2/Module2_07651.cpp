db483e79b66d88d845410a0d1d1327441ea95633
mikhaelagung <mikhaelagung3@gmail.com>
2023-10-28 22:11:40 +0700

    Tugas baru Module 2

--- Module2/Module2_07651.cpp
+++ Module2/Module2_07651.cpp
@@ -3,35 +3,36 @@ using namespace std;
 
 void arimatika_07651()
 {
-    int start;
-    int oper, result;
+    int start_07651, loops_07651;
+    int oper_07651, result;
 
-    cout << "masukan Angka yang ingin di gunakan: " << endl;
-    cin >> start;
+    cout << "masukan Angka yang ingin di gunakan: \n";
+    cin >> start_07651;
+    cout << "masukan Jumlah pengualangan hasil: ";
+    cin >> loops_07651;
 
     cout << "Baris aritmatika: ";
-    for (int i = 1; i <= 10; i++)
+    for (int i = 1; i <= loops_07651; i++)
     {
         if (i % 3 == 1)
         {
-            oper = i;
-            result = start + oper;
+            oper_07651 = i;
+            result = start_07651 + oper_07651;
         }
         else if (i % 3 == 2)
         {
-            oper = i;
-            result = start * oper;
+            oper_07651 = i;
+            result = start_07651 * oper_07651;
         }
         else
         {
-            oper = -i;
-            result = start + oper;
+            oper_07651 = -i;
+            result = start_07651 + oper_07651;
         }
         cout << result << " ";
-        start = result;
+        start_07651 = result;
     }
 }
-
 
 int main()
 {
--- Ujicoba.cpp
+++ Ujicoba.cpp
@@ -1,72 +1,47 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
-int main() {
-    int pilihan;
-    int hargaTotal = 0;
-
-    std::cout << "Menu Masakan RM Sederhana\n";
-    std::cout << "===========================\n";
-    std::cout << "1. Soto Ayam - Rp 15.000\n";
-    std::cout << "2. Soto Daging - Rp 15.000\n";
-    std::cout << "3. Rawon - Rp 20.000\n";
-    std::cout << "4. Gule - Rp 25.000\n";
-    std::cout << "Mau Pesan apa ?\n";
-    std::cin >> pilihan;
-
-    int tambahan = 0;
-    std::string tambahan2;
-
-    switch (pilihan) {
-        case 1:
-            std::cout << "Anda memesan Soto Ayam\n";
-            hargaTotal += 15000;
-            std::cout << "Mau tambah Telur ? 1 untuk ya dan 0 untuk tidak\n";
-            std::cin >> tambahan;
-            if (tambahan == 1) {
-                hargaTotal += 5000;
-                std::cout << "Anda memesan Soto Ayam dengan tambahan Telur\n";
+int main()
+{
+    int angka;
+    cout << "masukan inputan :" << endl;
+    cin >> angka;
+    for (int i = 1; i <= angka; i++)
+    {
+        int sum = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            sum += j;
+            cout << j;
+            if (j != i)
+            {
+                cout << " + ";
             }
-            else {
-                std::cout << "Anda tidak menambah apa-apa\n";
-            }
-            break;
-        case 2:
-            std::cout << "Anda memesan Soto Daging\n";
-            hargaTotal += 15000;
-            std::cout << "Mau tambah Telur ? 1 untuk ya dan 0 untuk tidak\n";
-            std::cin >> tambahan;
-            if (tambahan == 1) {
-                hargaTotal += 5000;
-                std::cout << "Anda memesan Soto Daging dengan tambahan Telur\n";
-            }
-            else {
-                std::cout << "Anda tidak menambah apa-apa\n";
-            }
-            break;
-        case 3:
-            std::cout << "Anda memesan Rawon\n";
-            hargaTotal += 20000;
-            std::cout << "Mau tambah Empal ? 'ya' untuk ya dan 'tidak' untuk tidak\n";
-            std::cin >> tambahan2;
-            if (tambahan2 == "ya") {
-                hargaTotal += 20000;
-                std::cout << "Anda memesan Rawon dengan tambahan Empal\n";
-            }
-            else {
-                std::cout << "Anda tidak menambah apa-apa\n";
-            }
-            break;
-        case 4:
-            std::cout << "Anda memesan Gule\n";
-            hargaTotal += 25000;
-            break;
-        default:
-            std::cout << "Pilihan tidak valid\n";
-            return 1; // Menghentikan program jika pilihan tidak valid.
+        }
+        cout << " = " << sum << endl;
     }
 
-    std::cout << "Total harga: Rp " << hargaTotal << std::endl;
+    cout << endl; 
+
+    for (int i = angka; i >= 1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+            if (j != 1)
+            {
+                cout << " * ";
+            }
+        }
+        cout << " = ";
+
+        int kali = 1;
+        for (int k = i; k >= 1; k--)
+        {
+            kali *= k;
+        }
+        cout << kali << endl;
+    }
 
     return 0;
 }
