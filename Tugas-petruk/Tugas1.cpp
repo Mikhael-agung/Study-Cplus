7c584e2670086325b517adfef3d027f3700dae20
mikhaelagung <mikhaelagung3@gmail.com>
2023-11-08 21:49:03 +0700

    Tugas petruk

---
+++ Tugas-petruk/Tugas1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "program Atm ";
+
+    // deklasrasi variable
+    int kode_kartu, pin;
+    int nomor_kartu = 18102007;
+    int password = 100000;
+    int ulang = 0;
+    
+
+    while ( ulang == 0){
+    cout << "masukan Nomor kartru anda : ";
+    cin >> kode_kartu;
+    cout << "masukan Kata sandi anda : ";
+    cin >> pin;
+
+    
+        if ((kode_kartu == nomor_kartu) && (pin == password)) {
+            cout << "masuk ke menu utama";
+        }
+    };
+    return 0;
+}
