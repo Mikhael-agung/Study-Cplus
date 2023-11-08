e4d2cf110d05b3d7b9734901cd00754b28a2381f
mikhaelagung <mikhaelagung3@gmail.com>
2023-11-08 23:16:44 +0700

    project pteruk

--- Tugas-petruk/Tugas1.cpp
+++ Tugas-petruk/Tugas1.cpp
@@ -3,32 +3,38 @@ using namespace std;
 
 int main()
 {
-    cout << "program Atm ";
-
-    // inilisdeklasrasi variable
+    // inisialiasai dan deklasrasi variable
     int kode_kartu, pin;
     int nomor_kartu = 18102007;
     int password = 100000;
     int ulang = 0;
-    int salah = 0;
 
-    while (ulang == 0)
+
+    // user memasukan pin
+    cout << "program Atm\n";
+    while (ulang < 3)
     {
         cout << "masukan Nomor kartru anda : ";
         cin >> kode_kartu;
         cout << "masukan Kata sandi anda : ";
         cin >> pin;
 
-        if ((kode_kartu == nomor_kartu) && (pin == password)) {
-            cout << "masuk ke menu utama";
+        if ((kode_kartu == nomor_kartu) && (pin == password))
+        {
+            cout << "masuk ke menu utama\n";
+            cout << "1. Transfer";
+            cout << "2. Exit";
         }
-        else{
-            salah = salah + 1;
-        } 
+        else
+        {
+            cout << "masukan Pin dengan benar";
+            ulang = ulang + 1;
+            if (ulang  == 3){
+                cout << "Kartu anda terblokir";
+            }
+        }
+    }
 
-        if (salah == 3 ){
-            cout << "Kartu anda terblokir";
-        }
-    };
+    cout << "Program berhenti";
     return 0;
 }
